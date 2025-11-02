a7e93b68cbfbe18cf47b7ba1fb6df0696986c192
Juan A. <adrephos@gmail.com>
2025-11-02 12:49:27 -0500

    feat: update footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -39,6 +41,8 @@ const config: Config = {
         blog: {
           showReadingTime: true,
           routeBasePath: "/",
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           feedOptions: {
             type: ["rss", "atom"],
             xslt: true,
@@ -54,9 +58,19 @@ const config: Config = {
     ],
   ],
 
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
+    },
+  ],
+
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    // image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
@@ -86,10 +100,6 @@ const config: Config = {
           title: "More",
           items: [
             {
-              label: "Blog",
-              to: "/",
-            },
-            {
               label: "Portfolio",
               href: "https://adrephos.com",
             },
@@ -99,12 +109,21 @@ const config: Config = {
             },
           ],
         },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/in/adrephos/",
+            },
+          ],
+        },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Adrephos. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.oneLight,
-      darkTheme: prismThemes.oneDark,
+      darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
 };
