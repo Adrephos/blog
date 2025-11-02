3a7421753154c7bd1f62880401361fa20a9db4a9
Juan A. <adrephos@gmail.com>
2025-11-01 21:48:32 -0500

    first commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,112 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Juan | Blog",
+  tagline: "継続は力なり",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://blog.adrephos.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  organizationName: "adrephos",
+  projectName: "blog",
+
+  onBrokenLinks: "throw",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: false,
+        blog: {
+          showReadingTime: true,
+          routeBasePath: "/",
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: "My Blog",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.png",
+      },
+      items: [
+        {
+          href: "https://adrephos.com",
+          label: "My portfolio",
+          position: "left",
+        },
+        {
+          href: "https://github.com/adrephos",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/",
+            },
+            {
+              label: "Portfolio",
+              href: "https://adrephos.com",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/adrephos",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Adrephos. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.oneLight,
+      darkTheme: prismThemes.oneDark,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ templates/post.md
@@ -0,0 +1,33 @@
+---
+slug: {{id}}
+description: This is my first post on Docusaurus.
+title: {{title}}
+date: {{date}}
+time: {{time}}
+authors: [adrephos]
+tags: []
+image: https://i.imgur.com/mErPwqL.png
+hide_table_of_contents: false
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet placerat
+ante, quis dignissim felis mattis eget. Fusce non vestibulum purus. Suspendisse
+libero dolor, porttitor at felis quis, aliquet luctus est. Praesent vestibulum
+vehicula dolor et tincidunt. Praesent nec cursus purus. Nunc sed tellus diam.
+Phasellus non dolor a quam pellentesque varius. Sed congue, urna in mollis
+luctus, eros nunc accumsan nisl, sit amet porta magna sem at tellus.
+
+<!--truncate -->
+
+:::info Not a real post
+Hello this is an info card
+:::
+
+## Subtitle
+
+Quisque pulvinar ornare leo. Donec et dui dignissim, vehicula magna quis, rutrum
+lacus. Aliquam ultrices pretium ante, at viverra massa efficitur quis. In
+molestie, ipsum sit amet interdum viverra, nisl ipsum dictum libero, ut volutpat
+tellus ligula a diam. Praesent blandit magna vel congue venenatis. Phasellus et
+posuere ipsum, luctus commodo sem. Nam tincidunt pellentesque congue. Proin
+tempor libero ut enim convallis imperdiet.
